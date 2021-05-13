b511e6e4e4b627b9756a86047a0fb445d2450e7a
Sujan Surkhali <68342268+Surkhali@users.noreply.github.com>
2021-05-13 16:15:37 +0545

    My first project for phython

---
+++ Mad-Libs-Generator.py
@@ -0,0 +1,24 @@
+print()
+print("---- Welcome to the Mad Libs Generator Game ----")
+print("---- By Sujan Surkhali ----")
+
+print()
+adj1 = input("Enter the 'Adjective': ")
+inven = input("Enter the 'An Invention': ")
+food = input("Enter the 'A Food': ")
+adj2 = input("Enter the 'Adjective': ")
+plural = input("Enter the 'Plural': ")
+adj3 = input("Enter the 'Adjective': ")
+pluralNoun1 = input("Enter the 'Plural Noun': ")
+pluralNoun2 = input("Enter the 'Plural Noun': ")
+print()
+
+print("------------ Ready for the magic ------------")
+print()
+print("I would like to say a few " + adj1 + " words about the most important invention of the twentieth century.\n"
+        "I am not referring to " + inven + " or even to the discovery of " + food + ". \nThe most " + adj2 + " invention,"
+        " in my opinion, is the sneaker. \nIf it were not for sneakers, our " + plural + " would be dirty, cold, and "
+        + adj3 + ". \nSneakers keep me from skidding if the " + pluralNoun1 + " are slippery, and when I run, \nthey "
+        "keep me from stubbing my " + pluralNoun2 + ".")
+
+
